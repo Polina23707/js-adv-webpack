b3df155d960684daa451af2e0c201ebfb9f770e5
Polina Gorelik <Polina23707@gmail.com>
2023-11-24 21:16:47 +0200

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
 // TODO: write your code here
-// import sum from './basic';
+import sum from './basic';
 
 console.log('app worked');
-
-// console.log(sum([1, 2]));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 
-const path = require('node:path');
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode:'production',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
   },
   module: {
     rules: [
@@ -25,14 +29,25 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+
     })
   ],
 };
